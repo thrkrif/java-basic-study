cb7ee8be6bb970e0e566667f88110cc48d99b062
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-09-06 22:42:24 +0900

    24/09/06

---
+++ src/construct/ConstructMain1.java
@@ -0,0 +1,15 @@
+package construct;
+
+public class ConstructMain1 {
+    public static void main(String[] args) {
+        MemberConstruct member1 = new MemberConstruct("학생1", 16, 80);
+        MemberConstruct member2 = new MemberConstruct("학생2", 17, 90);
+
+        MemberConstruct[] member = {member1,member2};
+
+        for (MemberConstruct m : member) {
+            System.out.println("이름: " + m.name + ", 나이: " + m.age + ", 성적: " + m.grade);
+        }
+
+    }
+}
---
+++ src/construct/MemberConstruct.java
@@ -0,0 +1,13 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    MemberConstruct(String name, int age, int grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
