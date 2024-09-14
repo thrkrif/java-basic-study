03c5eae16fc17f9e3b28f7d0fecf0e56ea2549a7
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-09-14 20:28:49 +0900

    24/09/14

---
+++ src/extends1/ex/Book.java
@@ -0,0 +1,20 @@
+package extends1.ex;
+
+public class Book extends  Item{
+    private  String author;
+    private  String isbn;
+
+    Book(String name, int price, String author, String isbn){
+        super(name,price);
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public void print(){
+        super.print();
+        System.out.println("- 저자: " + this.author + ", isbn: " + this.isbn);
+    }
+
+
+}
