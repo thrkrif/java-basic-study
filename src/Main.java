fa82a043964a7e4fb670d428c945c65cad6061d4
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-09-24 22:23:22 +0900

    24/09/24

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("김영한 자바 강의 코드들 한곳에 모아 깃허브에 올리기");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
